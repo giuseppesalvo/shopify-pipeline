7d78ae51b184a0bd5d02fca9690def7e1caf3053
Giuseppe <giuseppesalvo@outlook.it>
2018-02-14 11:13:29 +0100

    replaced local domain 'localhost' with current local ip in config/index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,38 @@
 const paths = require('./paths')
 const YAML = require('yamljs')
 
+function getIP() {
+
+	const ips = []
+
+	const ifaces = os.networkInterfaces();
+
+	Object.keys(ifaces).forEach(function (ifname) {
+	  var alias = 0;
+
+	  ifaces[ifname].forEach(function (iface) {
+	    if ('IPv4' !== iface.family || iface.internal !== false) {
+	      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+	      return
+	    }
+
+	    if (alias >= 1) {
+	      // this single interface has multiple ipv4 addresses
+	      ips.push(iface.address)
+	    } else {
+	      // this interface has only one ipv4 adress
+	      ips.push(iface.address)
+	    }
+	    ++alias;
+	  })
+	})
+
+	return ips
+}
+
 module.exports = {
   paths,
-  domain: 'https://localhost',
+  domain: `https://${getIP()[0]}`,
   port: 8080,
   regex: {
     images: /\.(png|svg|jpg|gif)$/,
